c8055e19d46baf871e11d6e5f32f7aad4ea1f72a
Afaq Nabi <afaqnabi@yahoo.com>
2023-10-22 17:13:13 -0600

    init

--- project/socialDistribution/urls.py
+++ project/socialDistribution/urls.py
@@ -6,12 +6,12 @@ from .views import *
 # Create a router and register our viewsets with it.
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'authors', AuthorViewSet, basename='author')
-router.register(r'posts', PostViewSet, basename='posts')
-router.register(r'comment', CommentViewSet, basename='comment')
+router.register(r'authors/<slug:field>/posts', PostViewSet, basename='posts')
+router.register(r'authors/<slug>/posts/<slug>/comment', CommentViewSet, basename='comment')
 router.register(r'followers', FollowViewSet, basename='followers')
 router.register(r'friend-request', FriendRequestViewSet,
                 basename='friend-request')
-router.register(r'like', LikeViewSet, basename='like')
+router.register(r'authors/<slug>/posts/<slug>/like', LikeViewSet, basename='like')
 router.register(r'connected-node', ConnectedNodeViewSet,
                 basename='connected-node')
 
