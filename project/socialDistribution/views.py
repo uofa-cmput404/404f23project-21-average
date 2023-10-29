4467fc0fa730d75b44305ccba58b0229ae70b090
GhunaymY <ghunaym@gmail.com>
2023-10-29 14:44:41 -0600

    Added a custom share with friends class, to ensure that image posts are only viewed by friends

--- project/socialDistribution/views.py
+++ project/socialDistribution/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import permissions, pagination, viewsets
 from .models import *
 from .serializers import *
+from django.db.models import Q
 
 # Create your views here.
 
@@ -16,11 +17,28 @@ class AuthorViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = Pagination
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            # Filter posts based on shared_with_friends and user relationship
+            return Post.objects.filter(
+                Q(shared_with_friends=False) | Q(owner__followers=user.author)
+            )
+        return Post.objects.filter(shared_with_friends=False)
+
+#Checks whether a user has permission to access a post based on the shared_with_friends attribute and the user's relationship with the author.
+class IsSharedWithFriends(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if obj.shared_with_friends and user.is_authenticated:
+            # Check if the user is a friend/follower of the author
+            return user.author.followers.filter(id=obj.owner.id).exists()
+        return not obj.shared_with_friends
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated,IsSharedWithFriends]
     pagination_class = Pagination
 
 
