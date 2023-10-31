3b7c86f5bd56c4e29ba01ef8461c9f646a9fbfe3
Afaq Nabi <afaqnabi@yahoo.com>
2023-10-31 03:21:47 -0600

    commit

--- project/settings.py
+++ project/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- project/wsgi.py
+++ project/wsgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
-pplication = DjangoWhiteNoise(application)
+pplication = WhiteNoise(application)
