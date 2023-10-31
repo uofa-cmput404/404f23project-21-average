1fec77c8491857717d51f25d749d85f6cd41e427
Afaq Nabi <afaqnabi@yahoo.com>
2023-10-31 17:49:59 -0600

    init

--- project/socialDistribution/urls.py
+++ project/socialDistribution/urls.py
@@ -5,43 +5,11 @@ from .views import AuthorViewSet, ImageViewSet, PostDetail, PostList, CommentVie
 from rest_framework_nested import routers
 
 # Create a router and register our viewsets with it.
-router = DefaultRouter(trailing_slash=False)
-router.register(r'authors', AuthorViewSet, basename='author')
-# router.register(r'posts',
-#                 PostViewSet, basename='posts')
-# router.register(r'comment',
-#                 CommentViewSet, basename='comment')
-# router.register(r'like',
-#                 LikeViewSet, basename='like')
-# router.register(r'followers', FollowViewSet, basename='followers')
-# router.register(r'friend-request', FriendRequestViewSet,
-#                 basename='friend-request')
-# router.register(r'connected-node', ConnectedNodeViewSet,
-#                 basename='connected-node')
-
-# nested routes
-# posts_router = routers.NestedSimpleRouter(
-#     router, r'authors', lookup='author')
-# posts_router.register(r'posts', PostViewSet, basename='author-posts')
-
-# comments_router = routers.NestedSimpleRouter(
-#     posts_router, r'posts', lookup='post')
-# comments_router.register(r'comments', CommentViewSet, basename='comments')
-
-# likes_router = routers.NestedSimpleRouter(
-#     posts_router, r'posts', lookup='like')
-# likes_router.register(r'likes', LikeViewSet, basename='likes')
-
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('', include(posts_router.urls)),
-    # path('', include(comments_router.urls)),
-    # path('', include(likes_router.urls)),
     path('authors/<slug:author_pk>/posts/', PostList.as_view(), name='posts'),
     path('authors/<slug:author_pk>/posts/<slug:post_pk>', PostDetail.as_view(), name='posts'),
     path('authors/<slug:author_pk>/posts/<slug:post_pk>/comments/', CommentViewSet.as_view(), name='comments'),
     path('authors/<slug:author_pk>/posts/<slug:post_pk>/image', ImageViewSet.as_view(), name='image'),
-    # path('posts/<slug:post_pk>/', PostViewSet.as_view(), name='posts'),
 ]
--- project/socialDistribution/views.py
+++ project/socialDistribution/views.py
@@ -46,6 +46,55 @@ class AuthorViewSet(viewsets.ModelViewSet):
     #     return Post.objects.filter(shared_with_friends=False)
 
 
+class AuthorListViewSet(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = Pagination
+    paginate_by_param = 'page_size'
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     if user.is_authenticated:
+    #         # Filter posts based on shared_with_friends and user relationship
+    #         return Post.objects.filter(
+    #             Q(shared_with_friends=False) | Q(owner__followers=user.author)
+    #         )
+    #     return Post.objects.filter(shared_with_friends=False)
+
+
+class AuthorDetailView(APIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = Pagination
+
+    @extend_schema(
+        tags=['authors'],
+    )
+    def get(self, request, author_pk, format=None):
+        author = get_object_or_404(Author, pk=author_pk)
+        serializer_context = {
+            'request': request,
+        }
+        serializer = AuthorSerializer(author, context=serializer_context)
+        return Response(serializer.data)
+
+    @extend_schema(
+        tags=['authors'],
+    )
+    def post(self, request, author_pk, format=None):
+        author = get_object_or_404(Author, pk=author_pk)
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
