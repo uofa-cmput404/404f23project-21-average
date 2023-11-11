738ff54a43d163a4c50e929efe0af71d0e3406d1
Afaq Nabi <afaqnabi@yahoo.com>
2023-11-11 15:50:45 -0700

    wip

--- backend/project/settings/production.py
+++ backend/project/settings/production.py
@@ -9,12 +9,12 @@ environ.Env.read_env()
 DEBUG = False
 DATABASES = {
     'default': {
-       "ENGINE": env("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": env("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
-        "USER": env("SQL_USER", "myuser"),
-        "PASSWORD": env("SQL_PASSWORD", "myuserpassword"),
-        "HOST": env("SQL_HOST", "localhost"),
-        "PORT": env("SQL_PORT", "3306"),
+       "ENGINE": 'postgres',
+        "NAME": 'SocialDistribution',
+        "USER": 'postgres',
+        "PASSWORD": 'postgres',
+        # "HOST": env("SQL_HOST", "localhost"),
+        # "PORT": env("SQL_PORT", "3306"),
     }
 }
 
