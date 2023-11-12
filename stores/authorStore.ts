293acbf5e3040c715519fb883ec4259d1910f1d7
Afaq Nabi <afaqnabi@yahoo.com>
2023-11-12 02:41:07 -0700

    wip

--- stores/authorStore.ts
+++ stores/authorStore.ts
@@ -7,7 +7,7 @@ export const useAuthorStore = defineStore({
     authorId: "",
     author: null,
     authToken: "",
-    BASE_URL: "https://cmput-average-21-b54788720538.herokuapp.com/api",
+    BASE_URL: "https://cmput-average-21-b54788720538.herokuapp.com",
   }),
   getters: {},
   actions: {
