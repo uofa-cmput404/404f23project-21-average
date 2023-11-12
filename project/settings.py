95ec4c090185641c954ce92592279498898ca91d
Afaq Nabi <afaqnabi@yahoo.com>
2023-11-11 20:07:30 -0700

    wip

--- project/settings.py
+++ project/settings.py
@@ -146,9 +146,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'build/static'),
-    os.path.join(BASE_DIR, 'dist'),
+    # os.path.join(BASE_DIR, 'dist'),
     os.path.join(BASE_DIR, '.output/public'),
-    os.path.join(BASE_DIR, '.nuxt'),
+    # os.path.join(BASE_DIR, '.nuxt'),
 ]
 TEMPLATE_DIRS = [
     os.path.join(BASE_DIR, 'templates/'),
