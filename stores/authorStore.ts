6a7cde44e12b373d11d5f23eb308701b3e55b3e8
Afaq Nabi <afaqnabi@yahoo.com>
2023-11-18 19:26:38 -0700

    wip

--- stores/authorStore.ts
+++ stores/authorStore.ts
@@ -7,7 +7,7 @@ export const useAuthorStore = defineStore({
     authorId: "",
     author: null,
     authToken: "",
-    BASE_URL: "http://127.0.0.1:8000",
+    BASE_URL: "http://127.0.0.1:8000/api",
   }),
   getters: {
     getAuthorId() {
