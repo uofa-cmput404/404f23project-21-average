5d15f101d825ede5c59400a3cbcea3093d2cdb79
Afaq Nabi <afaqnabi@yahoo.com>
2023-11-26 14:54:29 -0700

    changes

--- socialDistribution/urls.py
+++ socialDistribution/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
          PostDetail.as_view(), name='posts-detail'),
      path('posts/<slug:post_pk>/image/',
          ImageViewSet.as_view(), name='image'),
-     path('uthors/<slug:author_pk>/posts/stream', StreamPostList.as_view(), name='public-posts'),
+     path('authors/<slug:author_pk>/posts/stream', StreamPostList.as_view(), name='public-posts'),
 
     # comments
     path('authors/<slug:author_pk>/posts/<slug:post_pk>/comments/',
