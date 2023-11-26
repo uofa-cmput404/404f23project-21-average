dd3546c9deb83148437ea92486ee8cc0d03d8e63
Agrim Sood <agrimsood@gmail.com>
2023-11-26 14:43:36 -0700

    inbox works

--- pages/InboxPage.vue
+++ pages/InboxPage.vue
@@ -5,20 +5,21 @@
       <div class="header">INBOX</div>
       <div class="notification-list">
         <div v-for="notification in notifications" :key="notification.id" class="notification-item">
-          <div v-if="notification.type ==='post'">
-          <h3>{{ notification.type }}</h3>
-          <h3>{{ notification.author.username }}</h3>
-          <p>{{ notification.content }}</p>
-        </div>
-          <div v-if="notification.type === 'like'">
-            <h3>{{ notification.author }} liked your post</h3>
+          <div class="notification-content">
+            <div v-if="notification.type === 'post'">
+              <h3>{{ notification.author.username }}</h3>
+              <p>{{ notification.content }}</p>
             </div>
+            <div v-if="notification.type === 'like'">
+              <h3>{{ notification.summary }}</h3>
+            </div>
+          </div>
+          <h4 v-if="notification.type === 'post'" class="notification-type">{{ notification.type.toUpperCase() }}</h4>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 import axios from 'axios';
@@ -32,7 +33,6 @@ export default {
   data() {
     return {
       notifications: [], // This will hold the fetched notifications
-      likes:[]
     };
   },
 
@@ -42,7 +42,6 @@ export default {
       axios.defaults.headers.common["Authorization"] = `Basic ${authorStore.getAuthToken}`;
       const response = await axios.get(`${authorStore.BASE_URL}/authors/${authorStore.getAuthorId}/inbox/?page_size=100`);
 
-      // Assuming the response data is an array of notifications
       this.notifications = response.data.items;
       console.log(this.notifications)
     } catch (error) {
@@ -52,48 +51,58 @@ export default {
 };
 </script>
 
-  
-  <style scoped>
-  .app-container {
-    display: flex;
-    height: 100%;
-    width:100%;
-    position:fixed;
-    top:0;
-    bottom:0;
-    right:0;
-    background-color: black;
-  }
+<style scoped>
+.app-container {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: black;
+}
 
 .main-content {
-    position:fixed;
-    left:26%;
-    top:0;
-    bottom:0;
-    right:0;
-    background-color: #00C58E;
-    overflow-y: auto;
-    overflow-x:hidden;
-  }
-  
-  .header {
+  position: fixed;
+  left: 26%;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #00C58E;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.header {
   color: black;  
-  font-size: 30px;  /* Corrected the font size value */
+  font-size: 30px;
   text-align: center;
   margin-top: 20px;
   margin-bottom: 20px;
-  text-decoration: underline; /* Underlining the text */
-  font-weight: bolder; /* Making the text bold */
+  text-decoration: underline;
+  font-weight: bolder;
 }
 
-  
-  .notification-list .notification-item {
+.notification-list .notification-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
   background-color: black;
   margin: 10px auto;
   padding: 15px;
   border-radius: 10px;
   color: white;
   border: 1px solid #00C58E;
+}
+
+.notification-content {
+  flex-grow: 1;
+}
+
+.notification-type {
+  text-align: right;
+  /* Additional styling as needed */
 }
 
 .notification-list .notification-item h3 {
@@ -118,5 +127,4 @@ export default {
   background-color: #009B75;
   color: white;
 }
-
 </style>
