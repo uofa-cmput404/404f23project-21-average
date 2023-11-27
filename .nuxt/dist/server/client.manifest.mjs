099219473019862e2dbec99f95417c9fe3dcb22c
Afaq Nabi <afaqnabi@yahoo.com>
2023-11-27 15:01:03 -0700

    wip

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,18 +1,593 @@
 export default {
-  "@vite/client": {
-    "prefetch": true,
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "C:/Desktop/CMPUT 404 project/404f23project-21-average/node_modules/nuxt/dist/app/entry.js": {
+  "_authorStore.62a9e6f9.js": {
     "resourceType": "script",
     "module": true,
     "prefetch": true,
     "preload": true,
+    "file": "authorStore.62a9e6f9.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_nuxt-link.811c1244.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "nuxt-link.811c1244.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_vue.f36acd1f.62369b7e.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "vue.f36acd1f.62369b7e.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "error-404.7fc72018.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "error-404.7fc72018.css"
+    ],
+    "file": "error-404.9589a2a8.js",
+    "imports": [
+      "_nuxt-link.811c1244.js",
+      "_vue.f36acd1f.62369b7e.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.7fc72018.css": {
+    "file": "error-404.7fc72018.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "error-500.c5df6088.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "error-500.c5df6088.css"
+    ],
+    "file": "error-500.8f9e68a4.js",
+    "imports": [
+      "_vue.f36acd1f.62369b7e.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.c5df6088.css": {
+    "file": "error-500.c5df6088.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff": {
+    "resourceType": "font",
+    "mimeType": "font/woff",
+    "file": "bootstrap-icons.d0346eea.woff",
+    "src": "node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff"
+  },
+  "node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "bootstrap-icons.5b2dd4d4.woff2",
+    "src": "node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2"
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.0bc0c670.js",
     "isEntry": true,
-    "file": "C:/Desktop/CMPUT 404 project/404f23project-21-average/node_modules/nuxt/dist/app/entry.js"
+    "src": "node_modules/nuxt/dist/app/entry.js"
+  },
+  "pages/InboxPage.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "InboxPage.1c8e58c2.css",
+    "src": "pages/InboxPage.css"
+  },
+  "pages/InboxPage.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "InboxPage.1c8e58c2.css"
+    ],
+    "file": "InboxPage.2e528c77.js",
+    "imports": [
+      "_authorStore.62a9e6f9.js",
+      "pages/sidebar.vue",
+      "pages/postComponent.vue",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_nuxt-link.811c1244.js",
+      "pages/commentComponent.vue"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/InboxPage.vue"
+  },
+  "InboxPage.1c8e58c2.css": {
+    "file": "InboxPage.1c8e58c2.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/UserListPopup.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "UserListPopup.16da17aa.css",
+    "src": "pages/UserListPopup.css"
+  },
+  "pages/UserListPopup.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "UserListPopup.16da17aa.css"
+    ],
+    "file": "UserListPopup.cc36f84d.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/UserListPopup.vue"
+  },
+  "UserListPopup.16da17aa.css": {
+    "file": "UserListPopup.16da17aa.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/api.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "api.f2de91a3.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/api.vue"
+  },
+  "pages/app.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "app.0e7e8d23.js",
+    "imports": [
+      "_nuxt-link.811c1244.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/app.vue"
+  },
+  "pages/commentComponent.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "commentComponent.c7ad74b8.css",
+    "src": "pages/commentComponent.css"
+  },
+  "pages/commentComponent.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "commentComponent.c7ad74b8.css"
+    ],
+    "file": "commentComponent.cc82a3a8.js",
+    "imports": [
+      "_authorStore.62a9e6f9.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/commentComponent.vue"
+  },
+  "commentComponent.c7ad74b8.css": {
+    "file": "commentComponent.c7ad74b8.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/createPostComponent.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "createPostComponent.58c244d8.css",
+    "src": "pages/createPostComponent.css"
+  },
+  "pages/createPostComponent.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "createPostComponent.58c244d8.css"
+    ],
+    "file": "createPostComponent.ab90c97e.js",
+    "imports": [
+      "_authorStore.62a9e6f9.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/createPostComponent.vue"
+  },
+  "createPostComponent.58c244d8.css": {
+    "file": "createPostComponent.58c244d8.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/defualtprofilepic.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "defualtprofilepic.5917ac1f.jpg",
+    "src": "pages/defualtprofilepic.jpg"
+  },
+  "pages/editPostComponent.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "editPostComponent.cbaf87d1.css",
+    "src": "pages/editPostComponent.css"
+  },
+  "pages/editPostComponent.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "editPostComponent.cbaf87d1.css"
+    ],
+    "file": "editPostComponent.f545b071.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/editPostComponent.vue"
+  },
+  "editPostComponent.cbaf87d1.css": {
+    "file": "editPostComponent.cbaf87d1.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/friendComponent.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "friendComponent.47ce0d5c.css",
+    "src": "pages/friendComponent.css"
+  },
+  "pages/friendComponent.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "friendComponent.47ce0d5c.css"
+    ],
+    "file": "friendComponent.0598b8fd.js",
+    "imports": [
+      "_authorStore.62a9e6f9.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/friendComponent.vue"
+  },
+  "friendComponent.47ce0d5c.css": {
+    "file": "friendComponent.47ce0d5c.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/homePage.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "homePage.8233bc87.css",
+    "src": "pages/homePage.css"
+  },
+  "pages/homePage.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "homePage.8233bc87.css"
+    ],
+    "file": "homePage.8378bc77.js",
+    "imports": [
+      "_authorStore.62a9e6f9.js",
+      "pages/postComponent.vue",
+      "pages/sidebar.vue",
+      "node_modules/nuxt/dist/app/entry.js",
+      "pages/commentComponent.vue",
+      "_nuxt-link.811c1244.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/homePage.vue"
+  },
+  "homePage.8233bc87.css": {
+    "file": "homePage.8233bc87.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/loginPage.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "loginPage.3e8664c0.css",
+    "src": "pages/loginPage.css"
+  },
+  "pages/loginPage.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "loginPage.3e8664c0.css"
+    ],
+    "file": "loginPage.09b5c22b.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_authorStore.62a9e6f9.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/loginPage.vue"
+  },
+  "loginPage.3e8664c0.css": {
+    "file": "loginPage.3e8664c0.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/postComponent.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "postComponent.392a0515.css",
+    "src": "pages/postComponent.css"
+  },
+  "pages/postComponent.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "postComponent.392a0515.css"
+    ],
+    "file": "postComponent.11136b12.js",
+    "imports": [
+      "pages/commentComponent.vue",
+      "_authorStore.62a9e6f9.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/postComponent.vue"
+  },
+  "postComponent.392a0515.css": {
+    "file": "postComponent.392a0515.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/profilePage.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "profilePage.3e17f048.css",
+    "src": "pages/profilePage.css"
+  },
+  "pages/profilePage.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "assets": [
+      "defualtprofilepic.5917ac1f.jpg"
+    ],
+    "css": [
+      "profilePage.3e17f048.css"
+    ],
+    "file": "profilePage.f92f2691.js",
+    "imports": [
+      "pages/userPost.vue",
+      "pages/sidebar.vue",
+      "pages/commentComponent.vue",
+      "_authorStore.62a9e6f9.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "pages/UserListPopup.vue",
+      "_nuxt-link.811c1244.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/profilePage.vue"
+  },
+  "profilePage.3e17f048.css": {
+    "file": "profilePage.3e17f048.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "defualtprofilepic.5917ac1f.jpg": {
+    "file": "defualtprofilepic.5917ac1f.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  },
+  "pages/searchPage.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "searchPage.061bb993.css",
+    "src": "pages/searchPage.css"
+  },
+  "pages/searchPage.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "searchPage.061bb993.css"
+    ],
+    "file": "searchPage.44c9a30c.js",
+    "imports": [
+      "_authorStore.62a9e6f9.js",
+      "pages/sidebar.vue",
+      "pages/friendComponent.vue",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_nuxt-link.811c1244.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/searchPage.vue"
+  },
+  "searchPage.061bb993.css": {
+    "file": "searchPage.061bb993.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/sidebar.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "sidebar.12b26e6d.css",
+    "src": "pages/sidebar.css"
+  },
+  "pages/sidebar.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "assets": [
+      "bootstrap-icons.5b2dd4d4.woff2",
+      "bootstrap-icons.d0346eea.woff"
+    ],
+    "css": [
+      "sidebar.12b26e6d.css"
+    ],
+    "file": "sidebar.081d4429.js",
+    "imports": [
+      "_nuxt-link.811c1244.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/sidebar.vue"
+  },
+  "sidebar.12b26e6d.css": {
+    "file": "sidebar.12b26e6d.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "bootstrap-icons.5b2dd4d4.woff2": {
+    "file": "bootstrap-icons.5b2dd4d4.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "bootstrap-icons.d0346eea.woff": {
+    "file": "bootstrap-icons.d0346eea.woff",
+    "resourceType": "font",
+    "mimeType": "font/woff"
+  },
+  "pages/signupPage.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "signupPage.0ffa1c42.css",
+    "src": "pages/signupPage.css"
+  },
+  "pages/signupPage.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "signupPage.0ffa1c42.css"
+    ],
+    "file": "signupPage.ae9eac1b.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_authorStore.62a9e6f9.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/signupPage.vue"
+  },
+  "signupPage.0ffa1c42.css": {
+    "file": "signupPage.0ffa1c42.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/userPost.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "userPost.f6073175.css",
+    "src": "pages/userPost.css"
+  },
+  "pages/userPost.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "userPost.f6073175.css"
+    ],
+    "file": "userPost.942165ef.js",
+    "imports": [
+      "pages/commentComponent.vue",
+      "_authorStore.62a9e6f9.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/userPost.vue"
+  },
+  "userPost.f6073175.css": {
+    "file": "userPost.f6073175.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
   }
 }
