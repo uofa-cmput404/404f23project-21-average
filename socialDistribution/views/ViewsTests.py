cd41e366980acc5bc54b815ef764336c3258101f
GhunaymY <ghunaym@gmail.com>
2023-12-01 12:14:40 -0700

    Set Up

--- socialDistribution/views/ViewsTests.py
+++ socialDistribution/views/ViewsTests.py
@@ -6,6 +6,8 @@ from ..models import Author,Comment, Post, Follow, Inbox,PostLike, CommentLike
 import requests
 from unittest.mock import patch, MagicMock
 from datetime import datetime, timedelta
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
 
 #Testing for authorView.py
@@ -240,3 +242,22 @@ class InboxItemViewTest(TestCase):
 
 # Tests for Post View
 
+class PostViewTestCase(APITestCase):
+
+    def setUp(self):
+        # Set Up a User for Testing
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.author = Author.objects.create(user=self.user)
+        self.post_data = {
+            'title': 'Test Post',
+            'content': 'Test Content',
+            'visibility': 'PUBLIC',
+        }
+
+    def test_create_post(self):
+        # Test creating a new post
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(f'/authors/{self.author.id}/posts/', data=self.post_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    
