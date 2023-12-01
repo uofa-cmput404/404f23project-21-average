47b806cb0edd80b5c02c4bc4f724bd7d37bf5c3c
Afaq Nabi <afaqnabi@yahoo.com>
2023-11-30 22:39:49 -0700

    wip

--- socialDistribution/pagination.py
+++ socialDistribution/pagination.py
@@ -8,7 +8,7 @@ class Pagination(pagination.PageNumberPagination):
 
 class JsonObjectPaginator(pagination.PageNumberPagination):
     page_size = 5  # Adjust this based on your desired page size
-    page_size_query_param = 'page_size'
+    page_size_query_param = 'size'
     max_page_size = 100
 
     def get_paginated_response(self, data):
