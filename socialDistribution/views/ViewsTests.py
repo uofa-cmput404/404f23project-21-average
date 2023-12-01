b3087e52c895d0ab14d9154c6cf108a4fd8e1a33
GhunaymY <ghunaym@gmail.com>
2023-12-01 12:15:50 -0700

    Details

--- socialDistribution/views/ViewsTests.py
+++ socialDistribution/views/ViewsTests.py
@@ -266,4 +266,8 @@ class PostViewTestCase(APITestCase):
         response = self.client.get(f'/authors/{self.author.id}/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    
+    def test_get_single_post(self):
+        # Test getting details of a single post
+        post = Post.objects.create(author=self.author, title='Test Post', content='Test Content', visibility='PUBLIC')
+        response = self.client.get(f'/authors/{self.author.id}/posts/{post.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
