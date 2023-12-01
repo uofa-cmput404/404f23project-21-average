d7173e1c0b4887da091ae7902238b83b228e8d0e
GhunaymY <ghunaym@gmail.com>
2023-12-01 11:52:52 -0700

    Retrieve Events

--- socialDistribution/views/ViewsTests.py
+++ socialDistribution/views/ViewsTests.py
@@ -154,4 +154,16 @@ class GitHubViewTest(TestCase):
         # Set up any necessary test data
         self.client = APIClient()
         # Create a test author with a GitHub username
-        self.author = Author.objects.create(username='test_author', github='https://github.com/test_user') #Replace URL with a real user. 
+        self.author = Author.objects.create(username='test_author', github='https://github.com/test_user') # Replace URL with a real user. 
+
+    @patch('requests.get')
+    def test_get_github_events(self, mock_requests_get):
+        # Test retrieving GitHub events for an author
+        mock_response = mock_requests_get.return_value
+        mock_response.status_code = 200
+        mock_response.json.return_value = [{'event_type': 'PushEvent', 'created_at': '2023-01-01T12:00:00Z'}] # Setting up a mock HTTP response for a GitHub API request
+
+        response = self.client.post(reverse('github-events', args=[self.author.pk]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('event_type', response.data[0])
+        self.assertIn('created_at', response.data[0])
