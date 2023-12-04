9db75bb83169d0dee5f47dc7151f4ee42b1b9e1c
Afaq Nabi <afaqnabi@yahoo.com>
2023-12-04 15:49:23 -0700

    merge

--- socialDistribution/migrations/0022_auto_20231125_0735.py
+++ socialDistribution/migrations/0022_auto_20231125_0735.py
@@ -26,7 +26,7 @@ def generate_data(apps, schema_editor):
 
     # Use the default users for data generation
     for author in default_users:
-        for _ in range(5):
+        for _ in range(3):
             post = Post.objects.create(
                 title=fake.sentence(),
                 content=fake.paragraph(),
